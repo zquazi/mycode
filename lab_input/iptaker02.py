9a7f142b5b707bfa46e637fe02ada0ab3aabcf4a
zquazi <zubeda.quazi@verizon.com>
2019-05-13 17:13:33 -0400

    added student227,py3_shebang.py,no_shebang.py,with_shebang.py,iptaker01,lab_input/iptaker02exit

---
+++ lab_input/iptaker02.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+user_input = input("please enter an IPv4 IP address:")
+## the line below creates a single string that is passed to print()
+# print("You told me the IPv4 address is:" + user_input)
+
+## print() canbe given a series of objects seperated by a comma
+print("You told me the IPv4 address is:", user_input)
+
+## the line below asks for the vendor name asociated with the device
+user_input = input("please enter the vendor name for the device:")
+## print() canbe given a series of objects seperated by a comma
+print("You told me the vendor name is:", user_input)
+
