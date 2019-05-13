6fe014e2b775738ea49d18b0eb4f824963c4b0d0
zquazi <zubeda.quazi@verizon.com>
2019-05-13 19:01:58 -0400

    added mix-list3.py and animal0list.py

---
+++ mix-list3/animal-list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+list1 = ['Fox', 'Fly', 'Ant', 'Bee', 'Cod', 'Cat', 'Dog', 'Yak', 'Cow', 'Hen', 'Koi', 'Hog', 'Jay', 'Kit']
+print(list1)
+list1.extend(['Snake'])
+print(list1)
+list1.append(['Horse', 'Lion'])
+print(list1)
+print(list1[15])
+print(list1[15][1])
